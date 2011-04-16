09a416b1e9016f194c58158a12b8bcd0490269a1
Albert P. Linnell <linnell-at-astro.washington.edu>
2011-04-16 06:05:22 -0700

    default.mk: Optimization and OpenMP make options
      * setting ENV variable O=2 -> -O2 compiler optimizations
      * setting ENV variable MP=<anything> enables -fopenmp compiler feature
    
      (Still need to convert all makefiles, however, which I've been meaning
      to do, anyway.)
    
    Conflicts:
    
    	defaults.mk

--- defaults.mk
+++ defaults.mk
@@ -1,11 +1,19 @@
 ## Project defaults ##
 SHELL = /bin/sh
 ifeq ($(FC),f77)
-  export FC := gfortran
+  FC := gfortran
+  export FC
 endif
 ifeq ($(FFLAGS),)
 #  FFLAGS := -g -Wall
-  export FFLAGS := -g -ffpe-trap=invalid,zero,overflow -fbacktrace -fdump-core
+  FFLAGS := -g -ffpe-trap=invalid,zero,overflow -fbacktrace -fdump-core
+  ifeq ($(O),2)
+    FFLAGS := $(FFLAGS) -O2
+  endif
+  ifdef MP
+    FFLAGS := $(FFLAGS) -fopenmp
+  endif
+  export FFLAGS
 endif
 
 # Clear and set list of suffixes for implicit rules (which we
