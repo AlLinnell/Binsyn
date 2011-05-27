a985ae5d20b0b11017e34fdbf9b4ebbb428d4cfc
Paul R. DeStefano <-paul.destefano-(ta)willamettealumni.com>
2011-05-27 11:13:39 -0700

    Makefile Update: (PGC) consolidation and compiler hooks
      The purpose of this topic branch is to move move things to the master
      make file (and defaults.mk) and to add make options that allow for
      better control of compiler options.
    
      This patch is for PGC only and breaks all the other sub-program
      makefiles.  But it does include an updated Makefile.template which I
      will use later to update the other Makefiles.  Besides, the sub-program
      make files are nearly empty now since most things have been moved to the
      master Makefile and a new top level include called subRules.mk.

--- defaults.mk
+++ defaults.mk
@@ -16,8 +16,29 @@ ifeq ($(FFLAGS),)
   endif
   export FFLAGS
 endif
+## For now, ignore make errors, so that the whole project can
+## be compiled with a single make.
+ifeq ($(MAKEOPTS),)
+  MAKEOPTS := --ignore-errors
+endif
+
+srcFiles := $(wildcard *.F)
+includeFiles := $(wildcard *.FOR)
+objectFiles := $(srcFiles:.F=.o)
+# definitions required for building include/header dependencies
+#   a variable holding the grep command, can be used as a function
+grepSrcFiles = grep -l "^[^C].*INCLUDE.*$(1)" $(srcFiles)
+#   headerDepends.  Takes any header file as argument 1
+#   and returns the makefile rule with all the objects that 
+#   depend on that header file in one line.
+define headerDepends
+ $(subst .F,.o,$(shell $(call grepSrcFiles,$(1)))) : $(1)
+endef
+# get last compiled flags, and update if necessary
+lastFFLAGS := $(shell cat .lastCompileFlags)
 
 # Clear and set list of suffixes for implicit rules (which we
 # will probably not use).
 .SUFFIXES: 
 .SUFFIXES: .F .o
+
---
+++ subRules.mk
@@ -0,0 +1,48 @@
+## Default Rules for sub-programs ##
+
+## Include Global Defaults Makefile ##
+include ../defaults.mk
+ 
+# Build Main
+$(prog): $(objectFiles)
+	$(FC) $(FFLAGS) -o $(prog) $^
+
+# Build objects
+$(objectFiles): %.o:%.F 
+	$(FC) $(FFLAGS) -c $< -o $@
+
+# create phony dependancy if flags change
+ifneq ($(lastFFLAGS),$(FFLAGS))
+$(objectFiles): flagChange
+.PHONY: flagChange
+flagChange:
+	@echo "Compiler options changed, recompile needed."
+	@echo $(FFLAGS) > .lastCompileFlags
+endif
+
+# Include/Header File Dependencies
+# Calls headerDepends for very header file, and then evaluates the result as a makefile rule
+ifdef includeFiles
+  $(foreach header,$(includeFiles),$(eval $(call headerDepends,$(header))))
+endif
+
+# Other Actions
+.PHONY: clean cleanall cleanobj
+clean: cleanall
+cleanall:
+	-rm *.o $(prog)
+cleanobj:
+	-rm *.o
+
+# Debuging
+.PHONY: show
+show:
+	@echo command template to find header deps: $(grepSrcFiles)
+	@echo command to find COMA.FOR deps: $(call grepSrcFiles,COMA.FOR)
+	@echo files that depend on COMA.FOR: $(shell $(call grepSrcFiles,COMA.FOR))
+#	@echo $(subst .F,.o,$(shell $(call grepSrcFiles,COMA.FOR))) : COMA.FOR
+#	@echo $(call headerDepends,COMA.FOR)
+#	@echo $(call headerDepends,COMM.FOR)
+#	@echo $(call headerDepends,COMR.FOR)
+	@echo "last used flags:"\"$(lastFFLAGS)\"
+	@echo "current flags  :"\"$(FFLAGS)\"
